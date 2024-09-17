13de33043482a1464e692194a9d0c70df0ac4a9f
1dupuis <uaintscammingme2@gmail.com>
2024-09-17 08:19:33 -0400

    Update signup.js

--- account/signup.js
+++ account/signup.js
@@ -87,7 +87,7 @@ function setupAuthStateListener() {
             // User is signed in, redirect to dashboard or show welcome message
             showNotification(`Welcome, ${user.email}!`, 'success');
             // You can redirect to a dashboard page here
-            //window.location.href = 'classes/id';
+            window.location.href = 'https://dupuis.lol/classes/id';
         }
     });
 }
