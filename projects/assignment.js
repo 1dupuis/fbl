cb1604ab47cb7965a2de595e23ce722ede9c3da7
1dupuis <uaintscammingme2@gmail.com>
2024-09-19 18:47:58 -0400

    Update assignment.js

--- projects/assignment.js
+++ projects/assignment.js
@@ -15,8 +15,8 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 // Import Three.js and OrbitControls
-import * as THREE from 'https://unpkg.com/three@0.151.3/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.151.3/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Initialize Firebase (replace with your own config)
 const firebaseConfig = {
