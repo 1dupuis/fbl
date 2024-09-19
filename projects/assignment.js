52b54a44a1046057127f594345c8f0c544e6cb40
1dupuis <uaintscammingme2@gmail.com>
2024-09-18 20:09:43 -0400

    Update assignment.js

--- projects/assignment.js
+++ projects/assignment.js
@@ -20,13 +20,14 @@ import { OrbitControls } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r
 
 // Initialize Firebase (replace with your own config)
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAToB2gXmzCK4t-1dW5urnGG87gbK6MxR8",
+    authDomain: "dupuis-lol.firebaseapp.com",
+    databaseURL: "https://dupuis-lol-default-rtdb.firebaseio.com",
+    projectId: "dupuis-lol",
+    storageBucket: "dupuis-lol.appspot.com",
+    messagingSenderId: "807402660080",
+    appId: "1:807402660080:web:545d4e1287f5803ebda235",
+    measurementId: "G-TR8JMF5FRY"
 };
 
 const app = initializeApp(firebaseConfig);
